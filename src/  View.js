90b91c1570bd2ec32a027cb36c83af30499738e8
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:01:05 +0530

    react hook form with add.js

--- src/  View.js
+++ src/  View.js
@@ -4,6 +4,6 @@ function View() {
   const Delete = (id) => {
     toast('deleted successfully');
   };
-  return <div> </div>;
+  return <div > </div>;
 }
 export default View;
--- src/ Add.js
+++ src/ Add.js
@@ -1,10 +1,61 @@
 import react from 'react';
 import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
 
 function Add() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   const insert = () => {
     toast('new iten added');
   };
-  return <div> Add </div>;
+  return (
+    <div>
+      <h1> Add new students </h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register('name', { required: true })}
+          placeholder="name"
+          name="name"
+        />
+        <div> {errors.name && <span>name required</span>}</div>
+        <input
+          {...register('email', { required: true })}
+          placeholder="email"
+          name="email"
+        />
+        <div> {errors.email && <span>email required</span>}</div>
+        <input
+          {...register('phone', { required: true })}
+          placeholder="phone "
+          name="phone"
+        />
+        <div> {errors.phone && <span>phone required</span>}</div>
+        <input
+          {...register('password', { required: true })}
+          placeholder="password"
+          name="password"
+        />
+        <div> {errors.password && <span>password required</span>}</div>
+        <input
+          {...register('gender', { required: true })}
+          placeholder="gender"
+          name="gender"
+        />
+        <div> {errors.gender && <span>gender required</span>}</div>
+        <input
+          {...register('status', { required: true })}
+          placeholder="status"
+          name="status"
+        />
+        <div> {errors.status && <span>status required</span>}</div>
+        <button>Insert</button>
+      </form>
+    </div>
+  );
 }
 export default Add;

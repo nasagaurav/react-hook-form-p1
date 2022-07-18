4040c1b41f931e987ab2d0dc9ee5e97ccd2b99cf
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:12:23 +0530

    added table in view.js

--- src/  View.js
+++ src/  View.js
@@ -4,6 +4,28 @@ function View() {
   const Delete = (id) => {
     toast('deleted successfully');
   };
-  return <div > </div>;
+  return <div> <h1>All students?</h1>
+  <table>
+  <tr>
+    <th>id</th>
+    <th>name</th>
+    <th>email</th>
+    <th>phone</th>
+    <th>password</th>
+    <th>gender</th>
+    <th>status</th>
+
+  </tr>
+  <tr>
+    <td>?</td>
+    <td>item.name</td>
+    <td>item.email</td>
+    <td>item.phone</td>
+    <td>item.password</td>
+    <td>item.gender</td>
+    <td>item.status </td>
+  </tr>
+</table>
+</div>;
 }
 export default View;

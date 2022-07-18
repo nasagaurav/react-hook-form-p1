0b7b07258c7fcebb269fba1f35bb37e74c2be9cb
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:27:31 +0530

    delete lodic added in view.js

--- src/  View.js
+++ src/  View.js
@@ -1,31 +1,48 @@
 import react from 'react';
 import { toast } from 'react-toastify';
-function View() {
-  const Delete = (id) => {
-    toast('deleted successfully');
-  };
-  return <div> <h1>All students?</h1>
-  <table>
-  <tr>
-    <th>id</th>
-    <th>name</th>
-    <th>email</th>
-    <th>phone</th>
-    <th>password</th>
-    <th>gender</th>
-    <th>status</th>
+import {useSelector,useDispatch} from 'react-redux'
 
-  </tr>
-  <tr>
-    <td>?</td>
-    <td>item.name</td>
-    <td>item.email</td>
-    <td>item.phone</td>
-    <td>item.password</td>
-    <td>item.gender</td>
-    <td>item.status </td>
-  </tr>
-</table>
-</div>;
+function View() {
+  const state = useSelector(s=>s)
+  const dispatch = useDispatch();
+  const Delete = (index) => {
+  
+    toast('deleting id:${index}');
+    let temp = a.filter((x,1)=>!==index);
+    dispatch({type:'delete',payload:temp})
+  };
+  const a =state.a
+  return (
+    <div>
+      {' '}
+      <h1>All students {a.length}</h1>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>phone</th>
+          <th>password</th>
+          <th>gender</th>
+          <th>status</th>
+        </tr>
+        <tr>
+          {a.map((item,index))=>         
+          <td>{index}</td>
+          <td>item.name</td>
+          <td>item.email</td>
+          <td>item.phone</td>
+          <td>item.password</td>
+          <td>item.gender</td>
+          <td>item.status </td>
+          <td>
+
+            <button onClick={()=>del(index)}>Delete</button>
+          </td>
+        </tr>
+      }
+      </table>
+    </div>
+  );
 }
 export default View;

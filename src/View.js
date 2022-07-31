f68c8c7d6de729aec339cf21c78aaad4dc9f257b
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-31 08:05:00 +0530

    added table to View.js

--- src/View.js
+++ src/View.js
@@ -2,6 +2,30 @@ import React from 'react';
 import { toast } from 'react-toastify';
 function View() {
   const del = (id) => {};
-  return <div>View</div>;
+  return (
+    <div>
+      <h1>All students ?</h1>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>phone</th>
+          <th>password</th>
+          <th>gender</th>
+          <th>status</th>
+        </tr>
+        <tr>
+          <td>?</td>
+          <td>item.name</td>
+          <td>item.email</td>
+          <td>item.phone</td>
+          <td>item.password</td>
+          <td>item.gender</td>
+          <td>item.status</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 export default View;

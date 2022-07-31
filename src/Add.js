9078b41f3568a9ad0a7478d75d2d3ff40357744e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-31 07:52:51 +0530

    toast import to add and view

--- src/Add.js
+++ src/Add.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+
 function Add() {
+  const insert = () => {
+    toast('new item added');
+  };
   return <div>Add</div>;
 }
 export default Add;
--- src/View.js
+++ src/View.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 function View() {
+  const del = (id) => {
+    toast('deleted successfully');
+  };
   return <div>View</div>;
 }
 export default View;

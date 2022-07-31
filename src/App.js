bee32250041f03959fb5551987328da50386a28d
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-31 07:49:49 +0530

    basic setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Add from './Add';
+import View from './View';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Add />
+      <View />
     </div>
   );
 }
